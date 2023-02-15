93c69757dfb668057ae1541eef281b58e90c28e5
Millie-Jackson <millie.jackson1@gmail.com>
2023-02-15 14:51:52 +0000

    Replaces index before saving modified data to a .csv

--- Pokemon/pokemon.py
+++ Pokemon/pokemon.py
@@ -37,6 +37,37 @@ print(df.sort_values("Name", ascending=False))
 
 '''
 
+''' MODIFYING DATA
+
 # Adding up all numerical stats into a new Total column
+# 45+49+49+65+65+45 = 318
 df["Total"] = df["HP"] + df["Attack"] + df["Defense"] + df["Sp. Atk"] + df["Sp. Def"] + df["Speed"]
+df["Total"] = df.iloc[:, 4:10].sum(axis=1) # Same thing
 print(df.head(5))
+
+# Removes total column
+df = df.drop(columns=["Total"])
+print(df.head(5))
+
+# Reorder columns
+df = df[["Total", "HP", "Defense"]]
+cols = list(df.columns.values)
+df = df[cols[0:4] + [cols[-1]] + cols[4:12]] # Same thing
+
+'''
+
+
+# and = & or = 
+# Assign to new dataframe
+new_df = df.loc[(df["Type 1"] == "Grass") | 
+                (df["Type 2"] == "Poison") &
+                (df["HP"] > 70)]
+print(new_df.head(3))
+
+# Old index will still be there 
+# Re index and remove old index 
+new_df = new_df.reset_index(drop=True)
+
+# Save changed data to a new .csv
+df.to_csv("AirBnB/Pokemon/modified.csv", index=False) # Removing indexs is optional
+new_df.to_csv("AirBnB/Pokemon/filtered.csv", index=False)
--- tabular_data.py
+++ tabular_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
-def remove_rows_with_missing_ratings(df) -> df:
+
+
+def remove_rows_with_missing_ratings(df) -> None:
     '''Removes the rows with missing values. Takes a dataset as a dataframe. Returns a dataset as a dataframe'''
     return df 
 
-def combine_description_strings(df) -> df:
+def combine_description_strings(df) -> None:
     '''Combines the list of strings from the descriptions column into one string.
        Removes empty quotes.
        Removes records with empty discriptions.
@@ -13,30 +15,38 @@ def combine_description_strings(df) -> df:
        Returns a dataset as a dataframe.'''
     return df
 
-def set_default_feature_values(df) -> df:
+def set_default_feature_values(df) -> None:
     '''Replace empty values from guests, beds, bathrooms an bedrooms with 1.
        Takes in a dataset as a dataframe.
        Returns a dataset as a dataframe.'''
     return df
 
-def clean_tabular_data():
+def clean_tabular_data(df):
     '''Calls all the data cleaning functions on the tabular data.
        Takes in a raw dataframe.
        Returns processed data'''
     
-    remove_rows_with_missing_ratings()
-    combine_description_strings()
-    set_default_feature_values()
+    remove_rows_with_missing_ratings(df)
+    combine_description_strings(df)
+    set_default_feature_values(df)
+
+    # Re index and remove old index 
+    df = df.reset_index(drop=True, inplace=True)
 
     return
+
+
 
 if __name__ == "__main__":
     # Load the raw data
     df = pd.read_csv("AirBnB/Data/tabular_data/listing.csv")
 
     # Clean data
-    clean_tabular_data()
+    clean_tabular_data(df)
 
     # Save processed data as a .csv
+    df.to_csv("AirBnB/Data/tabular_data/clean_tabular_data.csv", index=False)
+
+
 
 # END OF FILE 
