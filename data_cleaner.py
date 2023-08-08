37b7fa93e2a67b8bfe4e818e8f6a2741cb289e86
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2023-08-08 10:31:44 +0100

    Refactor: Moved data cleaning functions into a data cleaning class

---
+++ data_cleaner.py
@@ -0,0 +1,128 @@
+import pandas as pd
+
+
+
+class DataCleaner:
+
+    """
+    A class for cleaning tabular data from a given CSV file.
+
+    This class encapsulates data cleaning operations for tabular data, including removing
+    rows with missing values in rating columns, combining and cleaning description strings,
+    and setting default feature values for missing entries.
+
+    Args:
+        filename (str): The path to the CSV file containing the tabular data.
+
+    Attributes:
+        filename (str): The path to the CSV file containing the tabular data.
+        df (pandas.DataFrame): The DataFrame containing the tabular data.
+
+    Methods:
+        remove_rows_with_missing_ratings(): Removes rows with missing values in rating columns.
+        combine_description_strings(): Combines and cleans the strings in the 'Description' column.
+        set_default_feature_values(): Sets default values for feature columns with missing values.
+        clean_tabular_data(): Performs a comprehensive data cleaning process on the DataFrame.
+
+    Example:
+        # Create an instance of DataCleaner
+        cleaner = DataCleaner("data.csv")
+
+        # Clean the tabular data
+        cleaner.clean_tabular_data()
+
+    """
+
+    def __init__(self, filename):
+        self.filename = filename 
+        self.df = None
+
+    def remove_rows_with_missing_ratings(self)-> None:
+    
+        """
+        Removes rows with missing values in the rating columns of the given dataframe.
+        
+        Returns:
+            None
+        """
+
+        # Put all the ratings columns in one dataframe
+        df_ratings = self.df[["Accuracy_rating", "Communication_rating", "Location_rating", "Check-in_rating", "Value_rating"]]
+    
+        # Force values into floats (not needed in airbnb but here for future databases)
+        df_ratings = df_ratings.apply(pd.to_numeric, errors='coerce')
+
+        # Remove missing values
+        self.df.dropna(subset=df_ratings.columns, inplace=True)
+
+        return None
+    
+    def combine_description_strings(self)-> None:
+
+        """
+        Combine and clean the strings in the 'Description' column of the given DataFrame.
+        
+        Returns:
+            None
+        """
+
+        # Removes missing descriptions
+        self.df.dropna(subset=["Description"], inplace=True)
+        # Removes "About this space" prefix
+        self.df["Description"] = self.df["Description"].str.replace("'About this space', ", "")
+        # Remove empty quotes from the lists
+        self.df["Description"] = self.df["Description"].str.replace(r"['\"]\s*['\"]", "", regex=True)
+
+        return 
+    
+    def set_default_feature_values(self)-> None:
+
+        """
+        Sets default values for the feature columns that contain missing values (NaN) in the DataFrame.
+        
+        Returns:
+            None
+        """
+
+        # Put all the feature columns in one dataframe
+        feature_columns = ["guests", "beds", "bathrooms", "bedrooms"]
+
+        # Replace all NaN with 1
+        for column in feature_columns:
+            self.df[column].fillna(1, inplace=True)
+        
+        return None
+    
+    def clean_tabular_data(self) -> pd.DataFrame:
+
+        """
+        Clean the tabular data in the given DataFrame.
+        
+        Returns:
+            None
+        """
+
+        # Load data
+        try:
+            self.df = pd.read_csv(self.filename)
+        except FileNotFoundError:
+            raise FileNotFoundError("Can't find data file")
+
+        df_before_update = self.df.copy()  # Make a copy of the original dataframe
+
+        self.remove_rows_with_missing_ratings()
+        self.combine_description_strings()
+        self.set_default_feature_values()
+
+        # Compare if 'df' has been modified after the update
+        is_updated = not self.df.equals(df_before_update)
+
+        if is_updated:
+            print("The original dataframe 'df' has been updated successfully.")
+        else:
+            print("The original dataframe 'df' remains unchanged.")
+
+        # Re index and remove old index 
+        self.df.reset_index(drop=True, inplace=True)
+
+        return self.df
--- tabular_data.py
+++ tabular_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import ast
+from data_cleaner import DataCleaner
 
 '''
 Functions:
@@ -8,133 +8,6 @@ Functions:
         set_default_feature_values(): Replace empty values from guests, beds, bathrooms an bedrooms with 1.
         clean_tabular_data(): Calls all the data cleaning functions on the tabular data.
 '''
-
-
-def remove_rows_with_missing_ratings(df)-> pd.DataFrame:
-    
-    """
-    Removes rows with missing values in the rating columns of the given dataframe.
-
-    Parameters:
-        df (pd.DataFrame): The input dataframe containing rating columns.
-
-    Returns:
-        pd.DataFrame: A modified dataframe with rows removed for missing values in the rating columns.
-    """
-
-    # Put all the ratings columns in one dataframe
-    df_ratings = df[["Accuracy_rating", "Communication_rating", "Location_rating", "Check-in_rating", "Value_rating"]]
-  
-    # Force values into floats (not needed in airbnb but here for future databases)
-    df_ratings = df_ratings.apply(pd.to_numeric, errors='coerce')
-
-    # Remove missing values
-    df.dropna(subset=df_ratings.columns, inplace=True)
-
-    return df
-
-def combine_description_strings(df)-> pd.DataFrame:
-
-    """
-    Combine and clean the strings in the 'Description' column of the given DataFrame.
-
-    This function removes missing descriptions (NaN),
-    removes the prefix "'About this space', 
-    and removes empty quotes from the lists in the 'Description' column.
-
-    Parameters:
-        df (pd.DataFrame): The input DataFrame containing the 'Description' column.
-
-    Returns:
-        pd.DataFrame: A modified DataFrame with cleaned 'Description' strings.
-
-    Notes:
-        - The changes are applied directly to the original DataFrame, modifying it in place.
-        - If the 'Description' column contains actual lists, the prefix removal and empty quote
-          removal might not be necessary; verify the format of the 'Description' values beforehand.
-    """
-
-    # Removes missing descriptions
-    df = df.dropna(subset=["Description"])
-    # Removes "About this space" prefix
-    df["Description"] = df["Description"].str.replace("'About this space', ", "")
-    # Remove empty quotes from the lists
-    df["Description"] = df["Description"].str.replace(r"['\"]\s*['\"]", "", regex=True)
-
-    return df
-
-def set_default_feature_values(df)-> pd.DataFrame:
-
-    """
-    Sets default values for the feature columns that contain missing values (NaN) in the DataFrame.
-
-    Args:
-        df (pandas.DataFrame): The input DataFrame containing feature columns.
-
-    Returns:
-        pandas.DataFrame: The modified DataFrame with the default feature values.
-
-    Raises:
-        None
-
-    """
-
-    # Put all the feature columns in one dataframe
-    df_features = df[["guests", "beds", "bathrooms", "bedrooms"]]
-
-    # Replace all NaN with 1
-    df_features = df_features.fillna(1)
-    
-    return df_features
-
-def clean_tabular_data(df) -> None:
-
-    """
-    Clean the tabular data in the given DataFrame.
-
-    This function performs the following operations on the DataFrame:
-    1. Makes a copy of the original DataFrame to keep track of changes.
-    2. Removes rows with missing values in the rating columns.
-    3. Combines and cleans the strings in the 'Description' column.
-    4. Sets default feature values to fill missing values in the DataFrame.
-    5. Compares if the DataFrame has been modified after the updates.
-    6. Displays a message indicating whether the original DataFrame has been updated.
-    7. Reindexes the DataFrame and removes the old index.
-
-    Parameters:
-        df (pd.DataFrame): The input DataFrame to be cleaned.
-
-    Returns:
-        pd.DataFrame: A modified DataFrame with cleaned data.
-
-    Notes:
-        - The changes are applied directly to the input DataFrame, modifying it in place.
-        - The function may print a message indicating whether the original DataFrame has
-          been updated after cleaning.
-
-    Example:
-        df = clean_tabular_data(df)
-
-    """
-
-    df_before_update = df.copy()  # Make a copy of the original dataframe
-
-    df = remove_rows_with_missing_ratings(df)
-    df = combine_description_strings(df)
-    df = set_default_feature_values(df)
-
-    # Compare if 'df' has been modified after the update
-    is_updated = not df.equals(df_before_update)
-
-    if is_updated:
-        print("The original dataframe 'df' has been updated successfully.")
-    else:
-        print("The original dataframe 'df' remains unchanged.")
-
-    # Re index and remove old index 
-    df.reset_index(drop=True, inplace=True)
-
-    return None
 
 def load_airbnb(label="Price_Night") -> pd.DataFrame:
 
@@ -179,17 +52,22 @@ def load_airbnb(label="Price_Night") -> pd.DataFrame:
 
 
 if __name__ == "__main__":
+
+    cleaner = DataCleaner("Data/tabular_data/listing.csv")
+    df = cleaner.clean_tabular_data()
+    
     # Load the raw data
-    df = pd.read_csv("Data/tabular_data/listing.csv")
+    #df = pd.read_csv("Data/tabular_data/listing.csv")
 
     # Clean data
-    clean_tabular_data(df)
+    #clean_tabular_data(df)
 
     # Save processed data as a .csv
     df.to_csv("Data/tabular_data/clean_tabular_data.csv", index=False)
 
     # Extract
     features, labels = load_airbnb()
+
 
 
 # END OF FILE 
