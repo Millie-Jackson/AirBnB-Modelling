c5c44e9009c86ef982c7a7e1ca3fd925547a2e37
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2025-01-16 16:54:05 +0000

    Delete docs/Learning Materials directory

--- docs/Learning Materials/13 Printing Things In Python/printing.py
+++
@@ -1,4 +0,0 @@
-print("This print statement was created in Python")
-
-age = 33
-print("Age: ", age)
--- docs/Learning Materials/26 Lists/Car Number Plates.py
+++
@@ -1,5 +0,0 @@
-
-number_plates = ["G06 WTR", "WL11 WFL", "QW68 PQR"]
-
-year = number_plates[0][1:3]
-year = int(year)
